4c752fa7c0db7d2bce8368dece5312d5369bbff0
Lascoyle <meursault45@live.com>
2021-01-18 23:38:05 +0100

    Add cards for dicplaying games informations in Games view

--- src/views/Games.vue
+++ src/views/Games.vue
@@ -1,12 +1,52 @@
 <template>
   <section id="games">
-      This is the Games View
+      <template>
+        <v-card class="mx-auto d-flex pa-5" v-for="(game, index) in games" :key="index">
+          <v-img :src="game.picture" contain :aspect-ratio="1" height="200px" class="col-lg-4"></v-img>
+          <div class="col-lg-8">
+            <v-card-title>{{ game.title }}</v-card-title>
+  
+            <v-card-subtitle>{{ game.platform }}</v-card-subtitle>
+  
+            <v-card-actions>
+              <v-btn color="orange lighten-2" text>Description</v-btn>
+  
+              <v-spacer></v-spacer>
+  
+              <v-btn icon @click="show = !show">
+                <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+              </v-btn>
+            </v-card-actions>
+            <v-expand-transition>
+            <div v-show="show">
+              <v-divider></v-divider>
+              <v-card-text>
+                {{ game.description }}
+              </v-card-text>
+            </div>
+          </v-expand-transition>
+          </div>
+        </v-card>
+      </template>
   </section>
 </template>
 
 <script>
+import axios from 'axios'
 
 export default {
-  name: 'Games'
+  name: 'Games',
+  data() {
+    return {
+      games: [],
+      show: false,
+    }
+  },
+
+  mounted() {
+    axios
+    .get('https://www.moogleapi.com/api/v1/games')
+    .then(response => (this.games = response.data))
+  }
 }
 </script>
